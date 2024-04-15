de167010a5f817dd7775fd70ae0c10c9fbcbbed7
Your Name <xlluan18@gmail.com>
2024-04-15 12:09:07 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import {useCounterStore} from '@/stores/counter'
+import{ onMounted } from 'vue'
+const counterStore = useCounterStore()
+console.log(counterStore)
+onMounted(() => {
+  counterStore.getList()
+ })
+</script>
+
+<template>
+ <button @click="counterStore.increment">
+  {{ counterStore.count }}
+
+ </button>
+ {{ counterStore.doubleCount }}
+
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
+  
